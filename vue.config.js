ba83fe0316380d42ffc8af1f24dffa8a53df9776
Alicia Sykes <sykes.alicia@gmail.com>
2021-06-23 15:34:18 +0100

    :hammer: Improved build experience, with progress bar and completion notification

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const WebpackBuildNotifierPlugin = require('webpack-build-notifier');
+
 // Get current version
 process.env.VUE_APP_VERSION = require('./package.json').version;
 
@@ -13,19 +16,28 @@ module.exports = {
         { test: /.svg$/, loader: 'vue-svg-loader' },
       ],
     },
+    plugins: [
+      new ProgressBarPlugin(),
+      new WebpackBuildNotifierPlugin({
+        title: 'Dashy Build Complete',
+        logo: './public/web-icons/dashy-logo.png',
+        suppressSuccess: true,
+        showDuration: true,
+      }),
+    ],
   },
 
   pwa: {
     name: 'Dashy',
-    manifestPath: './public/manifest.json',
+    manifestPath: './manifest.json',
     themeColor: '#00af87',
     msTileColor: '#0b1021',
     iconPaths: {
       manifestCrossorigin: 'use-credentials',
-      favicon64: './public/web-icons/favicon-64x64.png',
-      favicon32: './public/web-icons/favicon-32x32.png',
-      maskIcon: './public/web-icons/dashy-logo.png',
-      msTileImage: './public/web-icons/dashy-logo.png',
+      favicon64: './web-icons/favicon-64x64.png',
+      favicon32: './web-icons/favicon-32x32.png',
+      maskIcon: './web-icons/dashy-logo.png',
+      msTileImage: './web-icons/dashy-logo.png',
     },
   },
   pages: {
