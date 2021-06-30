361872cce628bed642b3749e7b652de4be916c45
Alicia Sykes <sykes.alicia@gmail.com>
2021-06-30 21:33:16 +0100

    :beers: Things and stuff

---
+++ docs/hello.md
@@ -0,0 +1,1 @@
+docusouris is stupid
--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -1,25 +1,45 @@
 import React from 'react';
 import './../styles/HomepageFeatures.scss';
+import Button from '../components/Button';
+
+import IconAuth from '../../static/icons/features_authentication.svg';
+import IconCloudSync from '../../static/icons/features_cloud-sync.svg';
+import IconDeploy from '../../static/icons/features_depoloyment.svg';
+import IconIconography from '../../static/icons/features_icons.svg';
+import IconLayout from '../../static/icons/features_layout-customization.svg';
+import IconOpeningMethods from '../../static/icons/features_opening-methods.svg';
+import IconShortcuts from '../../static/icons/features_shortcuts.svg';
+import IconStatusIndicators from '../../static/icons/features_status-indicators.svg';
+import IconThemes from '../../static/icons/features_themes.svg';
+import IconUiConfig from '../../static/icons/features_ui-configuration.svg';
 
 const FeatureList = [
   {
     title: 'Search & Shortcuts',
     description: (
       <>
-        Quisque ut dolor gravida, placerat libero vel, euismod. Ambitioni dedisse
-        scripsisse iudicaretur.Donec sed odio operae, eu vulputate felis rhoncus.
-        Nihilne te nocturnum praesidium Palati, nihil urbis vigiliae.
+        Arguably one of the most important features for any start page is the ability to find
+        and launch applications as quickly as possible.
+        <br />
+        To filter services, just start typing. No need to select the search bar or use any special key.
+        You can then use either the tab key or arrow keys to select and move between results,
+        and hit enter to launch the currently selected application.
       </>
     ),
+    icon: (<IconShortcuts />),
   },
   {
     title: 'Theming',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Dashy comes with a ton of built-in themes, so you'll easily find one that suits you.
+        But it's also easy to write you're own. All colors, and most other CSS properties
+        make use of CSS variables, which makes customizing the look and feel of Dashy very easy.
+        You can apply custom styles directly through the UI,
+        or pass an external stylesheet into the config file
       </>
     ),
+    icon: (<IconThemes />),
   },
   {
     title: 'Customizable Layouts',
@@ -29,16 +49,30 @@ const FeatureList = [
         be extended while reusing the same header and footer.
       </>
     ),
+    icon: (<IconLayout />),
   },
 ];
 
-function Feature({ title, description, index }) {
+const getColor = (index) => {
+  const remainder = index % 4;
+  switch (remainder) {
+    case 0: return 'pink';
+    case 1: return 'blue';
+    case 2: return 'green';
+    case 3: return 'yellow';
+    default: return 'white';
+  }
+};
+
+function Feature({ title, description, icon, index }) {
   const side = index % 2 == 0 ? 'left' : 'right';
+  const color = getColor(index)
   return (
-    <div className={`feature align-${side}`}>
+    <div className={`feature align-${side} color-${color}`}>
       <div className="feature-half">
-        <h3>{title}</h3>
+        <div className="feature-title">{icon}<h3>{title}</h3></div>
         <p>{description}</p>
+        <Button to="/docs" color={color}>{icon} Docs</Button>
       </div>
       <div className="feature-half">
         <p>Static Asset will go here</p>
