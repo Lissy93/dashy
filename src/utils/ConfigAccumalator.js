a27316d5978642241a67cbec5db9e0b24f393bbe
Alicia Sykes <sykes.alicia@gmail.com>
2021-07-04 09:00:53 +0100

    :recycle: Refactors the config accumulator into a Class

--- src/utils/ConfigAccumalator.js
+++ src/utils/ConfigAccumalator.js
@@ -3,73 +3,78 @@
  * Also ensures that any missing attributes are populated with defaults, and the
  * object is structurally sound, to avoid any error if the user is missing something
  * The main config object is made up of three parts: appConfig, pageInfo and sections
+ * For anything other than these three sections, please see @utils/ConfigHelpers.js
  */
-import Defaults, { localStorageKeys } from '@/utils/defaults';
+import {
+  localStorageKeys,
+  appConfig as defaultAppConfig,
+  pageInfo as defaultPageInfo,
+  iconSize as defaultIconSize,
+  layout as defaultLayout,
+} from '@/utils/defaults';
+
 import conf from '../../public/conf.yml';
 
-/**
- * Returns the appConfig section, as JSON
- */
-export const appConfig = (() => {
-  const appConfigFile = conf.appConfig || {};
-  let usersAppConfig = Defaults.appConfig;
-  if (localStorage[localStorageKeys.APP_CONFIG]) {
-    usersAppConfig = JSON.parse(localStorage[localStorageKeys.APP_CONFIG]);
-  } else if (appConfigFile !== {}) {
-    usersAppConfig = appConfigFile;
+export default class ConfigAccumulator {
+  constructor() {
+    this.conf = conf;
   }
-  usersAppConfig.layout = localStorage[localStorageKeys.LAYOUT_ORIENTATION]
-    || appConfigFile.layout || Defaults.layout;
-  usersAppConfig.iconSize = localStorage[localStorageKeys.ICON_SIZE]
-    || appConfigFile.iconSize || Defaults.iconSize;
-  return usersAppConfig;
-})();
 
-/**
- * Returns the pageInfo section, as JSON
- */
-export const pageInfo = (() => {
-  const defaults = Defaults.pageInfo;
-  let localPageInfo;
-  try {
-    localPageInfo = JSON.parse(localStorage[localStorageKeys.PAGE_INFO]);
-  } catch (e) {
-    localPageInfo = {};
-  }
-  const pi = conf.pageInfo || defaults; // The page info object to return
-  pi.title = localPageInfo.title || conf.pageInfo.title || defaults.title;
-  pi.description = localPageInfo.description || conf.pageInfo.description || defaults.description;
-  pi.navLinks = localPageInfo.navLinks || conf.pageInfo.navLinks || defaults.navLinks;
-  pi.footerText = localPageInfo.footerText || conf.pageInfo.footerText || defaults.footerText;
-  return pi;
-})();
-
-/**
- * Returns the sections section, as an array of JSON objects
- */
-export const sections = (() => {
-  // If the user has stored sections in local storage, return those
-  const localSections = localStorage[localStorageKeys.CONF_SECTIONS];
-  if (localSections) {
-    try {
-      const json = JSON.parse(localSections);
-      if (json.length >= 1) return json;
-    } catch (e) {
-      // The data in local storage has been malformed, will return conf.sections instead
+  /* App Config */
+  appConfig() {
+    const appConfigFile = this.conf.appConfig || {};
+    let usersAppConfig = defaultAppConfig;
+    if (localStorage[localStorageKeys.APP_CONFIG]) {
+      usersAppConfig = JSON.parse(localStorage[localStorageKeys.APP_CONFIG]);
+    } else if (appConfigFile !== {}) {
+      usersAppConfig = appConfigFile;
     }
+    usersAppConfig.layout = localStorage[localStorageKeys.LAYOUT_ORIENTATION]
+      || appConfigFile.layout || defaultLayout;
+    usersAppConfig.iconSize = localStorage[localStorageKeys.ICON_SIZE]
+      || appConfigFile.iconSize || defaultIconSize;
+    return usersAppConfig;
   }
-  // If the function hasn't yet returned, then return the config file sections
-  return conf.sections;
-})();
 
-/**
- * Returns the complete configuration, as JSON
- */
-export const config = (() => {
-  const result = {
-    appConfig,
-    pageInfo,
-    sections,
-  };
-  return result;
-})();
+  /* Page Info */
+  pageInfo() {
+    const defaults = defaultPageInfo;
+    let localPageInfo;
+    try {
+      localPageInfo = JSON.parse(localStorage[localStorageKeys.PAGE_INFO]);
+    } catch (e) {
+      localPageInfo = {};
+    }
+    const pi = this.conf.pageInfo || defaults; // The page info object to return
+    pi.title = localPageInfo.title || conf.pageInfo.title || defaults.title;
+    pi.description = localPageInfo.description || conf.pageInfo.description || defaults.description;
+    pi.navLinks = localPageInfo.navLinks || conf.pageInfo.navLinks || defaults.navLinks;
+    pi.footerText = localPageInfo.footerText || conf.pageInfo.footerText || defaults.footerText;
+    return pi;
+  }
+
+  /* Sections */
+  sections() {
+    // If the user has stored sections in local storage, return those
+    const localSections = localStorage[localStorageKeys.CONF_SECTIONS];
+    if (localSections) {
+      try {
+        const json = JSON.parse(localSections);
+        if (json.length >= 1) return json;
+      } catch (e) {
+        // The data in local storage has been malformed, will return conf.sections instead
+      }
+    }
+    // If the function hasn't yet returned, then return the config file sections
+    return this.conf.sections;
+  }
+
+  /* Complete config */
+  config() {
+    return {
+      appConfig: this.appConfig(),
+      pageInfo: this.pageInfo(),
+      sections: this.sections(),
+    };
+  }
+}
