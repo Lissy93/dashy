f7c159a0a2398122c9d62d716d17d28c5ab20974
Alicia Sykes <sykes.alicia@gmail.com>
2021-07-04 19:33:29 +0100

    :construction: Small change to Error Handler, WIP

--- src/utils/ErrorHandler.js
+++ src/utils/ErrorHandler.js
@@ -4,8 +4,8 @@
  * Function called when an error happens
  * If you wish to use an error logging service, put code for it here
  */
-const ErrorHandler = function handler(msg) {
-  console.warn(msg);
+const ErrorHandler = function handler(msg, error = 'No stack trace provided') {
+  console.warn(msg, error);
 };
 
 export default ErrorHandler;
