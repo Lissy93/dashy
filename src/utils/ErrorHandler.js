ad069bdf308c1c96b35de76ba10e6c1ee3f72c4d
Alicia Sykes <sykes.alicia@gmail.com>
2021-09-11 01:34:48 +0100

    :zap: Adds filter key to error handler

--- src/utils/ErrorHandler.js
+++ src/utils/ErrorHandler.js
@@ -4,11 +4,13 @@ import { warningMsg } from '@/utils/CoolConsole';
 
 /**
  * Function called when an error happens
- * If you wish to use an error logging service, put code for it here
+ * Will call to function which prints helpful message to console
+ * If error reporting is enabled, will also log the message to Sentry
+ * If you wish to use your own error logging service, put code for it here
  */
 const ErrorHandler = function handler(msg) {
   warningMsg(msg);
-  Sentry.captureMessage(msg);
+  Sentry.captureMessage(`[USER-WARN] ${msg || 'Uncaptured Message'}`);
 };
 
 export default ErrorHandler;
