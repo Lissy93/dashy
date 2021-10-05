2946412b4c7da84583575cf4d7e1b737c55dc121
Alicia Sykes <sykes.alicia@gmail.com>
2021-10-05 20:25:10 +0100

    :construction: WIP, working on VueX store

---
+++ src/store.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+  },
+});
+
+export default store;
