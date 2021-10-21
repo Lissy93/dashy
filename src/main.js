fbbc5e075328f8eff10d97d287a14edaab00cd04
Alicia Sykes <sykes.alicia@gmail.com>
2021-10-22 00:27:38 +0100

    :heavy_plus_sign: Imports Vue-JSON-tree component

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import VModal from 'vue-js-modal';      // Modal component
 import VSelect from 'vue-select';       // Select dropdown component
 import VTabs from 'vue-material-tabs';  // Tab view component, used on the config page
 import Toasted from 'vue-toasted';      // Toast component, used to show confirmation notifications
+import TreeView from 'vue-json-tree-view';
 
 // Import base Dashy components and utils
 import Dashy from '@/App.vue';          // Main Dashy Vue app
@@ -27,6 +28,7 @@ Vue.use(VueI18n);
 Vue.use(VTooltip, tooltipOptions);
 Vue.use(VModal);
 Vue.use(VTabs);
+Vue.use(TreeView);
 Vue.use(Toasted, toastedOptions);
 Vue.component('v-select', VSelect);
 Vue.directive('clickOutside', clickOutside);
