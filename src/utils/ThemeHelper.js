1f3ed135deb9d3b2247d95e16c02a73dfc1f7a11
Alicia Sykes <sykes.alicia@gmail.com>
2022-03-26 19:34:37 +0000

    :rotating_light: Adds missing semmi

--- src/utils/ThemeHelper.js
+++ src/utils/ThemeHelper.js
@@ -32,7 +32,7 @@ export const LoadExternalTheme = function th() {
   const preloadTheme = (href) => {
     const link = document.createElement('link');
     link.rel = 'stylesheet';
-    link.type = 'text/css'
+    link.type = 'text/css';
     link.href = href;
     document.head.appendChild(link);
     return new Promise((resolve, reject) => {
