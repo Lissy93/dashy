71291c1ce9de694e350f6bde2aabe7e5cc3e90a7
Alicia Sykes <alicia@omg.lol>
2022-12-09 20:55:49 +0000

    ✨ Adds update support to image widget (#992)

--- src/components/Widgets/ImageWidget.vue
+++ src/components/Widgets/ImageWidget.vue
@@ -9,10 +9,24 @@ import WidgetMixin from '@/mixins/WidgetMixin';
 
 export default {
   mixins: [WidgetMixin],
+  data: () => ({
+    updateCount: 0,
+  }),
   computed: {
+    /* The path to image to render */
     imagePath() {
       if (!this.options.imagePath) this.error('You must specify an imagePath');
-      return this.options.imagePath;
+      return `${this.options.imagePath}${this.updatePathParam}`;
+    },
+    /* Generate a URL param, to be updated in order to re-fetch image */
+    updatePathParam() {
+      return this.updateCount ? `#dashy-update-${this.updateCount}` : '';
+    },
+  },
+  methods: {
+    /* In order to re-fetch the image, we much update the URL with an arbitrary hash */
+    update() {
+      this.updateCount += 1;
     },
   },
 };
