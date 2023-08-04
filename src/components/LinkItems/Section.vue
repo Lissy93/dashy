c0285d410e3bbb887a870618db9537c2771fa671
Shawn Salat <31082405+ThinkSalat@users.noreply.github.com>
2023-08-04 17:23:59 -0600

    use static iframe ref name

--- src/components/LinkItems/Section.vue
+++ src/components/LinkItems/Section.vue
@@ -74,8 +74,8 @@
     </div>
     <!-- Modal for opening in modal view -->
     <IframeModal
-      :ref="`iframeModal-${groupId}`"
-      :name="`iframeModal-${groupId}`"
+      :ref="`iframeModal`"
+      :name="`iframeModal`"
       @closed="$emit('itemClicked')"
     />
     <!-- Edit item menu -->
@@ -213,7 +213,7 @@ export default {
   methods: {
     /* Opens the iframe modal */
     triggerModal(url) {
-      this.$refs[`iframeModal-${this.groupId}`].show(url);
+      this.$refs.iframeModal.show(url);
     },
     /* Sorts items alphabetically using the title attribute */
     sortAlphabetically(items) {
