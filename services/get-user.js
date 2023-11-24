821ed3433bd398240a1743a7539417346c53f20d
Todd E Johnson <todd@toddejohnson.net>
2023-11-24 00:11:04 -0600

    Add Proxy IP check for header security

--- services/get-user.js
+++ services/get-user.js
@@ -2,8 +2,12 @@ module.exports = (config, req) => {
   try {
     if ( config.appConfig.auth.enableHeaderAuth ) {
       const userHeader = config.appConfig.auth.headerAuth.userHeader;
-      return { "success": true, "user": req.headers[userHeader.toLowerCase()] };
+      const proxyWhitelist = config.appConfig.auth.headerAuth.proxyWhitelist;
+      if ( proxyWhitelist.includes(req.socket.remoteAddress) ) {
+        return { "success": true, "user": req.headers[userHeader.toLowerCase()] };
+      }
     }
+    return {};
   } catch (e) {
     console.warn("Error get-user: ", e);
     return { 'success': false };
