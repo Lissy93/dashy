8eba317ff60eea1e81bba2f7607c482e150d957b
Alicia Sykes <alicia@omg.lol>
2024-01-08 10:09:51 +0000

    Carbon

--- src/components/CarbonAd.js
+++ src/components/CarbonAd.js
@@ -1,14 +1,6 @@
 import React, { useEffect } from 'react';
 
 const CarbonAds = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = '//cdn.carbonads.com/carbon.js?serve=CWYIC53L&placement=dashyto';
-    script.id = '_carbonads_js';
-    script.async = true;
-    document.body.appendChild(script);
-  }, []);
-
   return (
     <>
     <div id="carbonads"></div>
