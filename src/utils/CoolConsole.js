938222e1ef0ae2b01c10fc9f68bcd18beda6e074
Alicia Sykes <alicia@omg.lol>
2024-02-27 20:16:14 +0000

    💡 Updates error log

--- src/utils/CoolConsole.js
+++ src/utils/CoolConsole.js
@@ -9,7 +9,7 @@ export const welcomeMsg = () => {
 /* Prints warning message, usually when there is a configuration error */
 export const warningMsg = (message, stack) => {
   console.info(
-    `\n%c⚠️ Warning ⚠️%c \n${message} \n\n%cThis is likely not an issue with Dashy, but rather your configuration. If you think it is a bug, please open a ticket on GitHub: https://git.io/JukXk`,
+    `\n%c⚠️ Warning ⚠️%c \n${message} \n\n%cThis is likely not an issue with Dashy, but rather your configuration.\nIf you think it is a bug, please open a ticket on GitHub: https://git.io/JukXk`,
     "color:#ceb73f; background: #ceb73f33; font-size:1.5rem; padding:0.15rem; margin: 1rem auto; font-family: Rockwell, Tahoma, 'Trebuchet MS', Helvetica; border: 2px solid #ceb73f; border-radius: 4px; font-weight: bold; text-shadow: 1px 1px 1px #000000bf;",
     'font-weight: bold; font-size: 1rem;color: #ceb73f;',
     "color: #ceb73f; font-size: 0.75rem; font-family: Tahoma, 'Trebuchet MS', Helvetica;",
