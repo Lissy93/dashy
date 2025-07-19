806db58be62fb15eded86c65a5b25acbc844bebe
Al Saleh <alayham@alayham.com>
2025-07-19 11:40:50 +0200

    send json response for status check

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ const app = express()
   .use(ENDPOINTS.statusCheck, (req, res) => {
     try {
       statusCheck(req.url, async (results) => {
-        await res.end(results);
+        await res.json(results).end();
       });
     } catch (e) {
       printWarning(`Error running status check for ${req.url}\n`, e);
