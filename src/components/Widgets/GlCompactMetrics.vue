98f4a38683b67c83db122ce7154bcf767ff5e356
jx163 <jx163@students.waikato.ac.nz>
2025-10-02 11:54:02 +1300

    fix lint issues

--- src/components/Widgets/GlCompactMetrics.vue
+++ src/components/Widgets/GlCompactMetrics.vue
@@ -325,7 +325,7 @@ export default {
     },
 
     formatBytes(bytes) {
-      if (!bytes || Number.isNaN(bytes)) return '0 B';
+      if (bytes == null || isNaN(bytes)) return '0 B';
       const k = 1024;
       const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
       const i = Math.floor(Math.log(bytes) / Math.log(k));
