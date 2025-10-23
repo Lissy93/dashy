772026a17f17c83811dc71dea44b6e67034c92d9
Peter Gyorfi <hello@apgyorfi.dev>
2025-10-23 22:10:14 +0200

    🇭🇺 Add Hungarian language support

--- src/utils/languages.js
+++ src/utils/languages.js
@@ -10,6 +10,7 @@ import el from '@/assets/locales/el.json';
 import es from '@/assets/locales/es.json';
 import fr from '@/assets/locales/fr.json';
 import hi from '@/assets/locales/hi.json';
+import hu from '@/assets/locales/hu.json';
 import it from '@/assets/locales/it.json';
 import ja from '@/assets/locales/ja.json';
 import ko from '@/assets/locales/ko.json';
@@ -97,6 +98,12 @@ export const languages = [
     code: 'hi',
     locale: hi,
     flag: '🇮🇳',
+  },
+  { // Hungarian
+    name: 'Magyar',
+    code: 'hu',
+    locale: hu,
+    flag: '🇭🇺',
   },
   { // Italian
     name: 'Italiano',
